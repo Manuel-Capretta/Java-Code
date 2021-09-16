0cb369219f589b3eb2ea57313c55a3613e251988
Manuel Capretta <familiecapretta@gmail.com>
2021-09-16 10:59:52 +0200

    first push

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int result = mult(2, 3);
+        System.out.println(result);
+    }
+
+    public static int mult(int num1, int num2){
+        return num1*num2;
+    }
+}
