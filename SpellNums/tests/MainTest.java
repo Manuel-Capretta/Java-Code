15d6b2af97cb15d2ebdb8e91753800816c4b2009
Manuel Capretta <familiecapretta@gmail.com>
2021-09-23 11:32:50 +0200

    test method renamed

--- SpellNums/tests/MainTest.java
+++ SpellNums/tests/MainTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void spell_testForRightWord(){
+    void spellTestForRightWord(){
         assertEquals("sieben hundert sechs und vierzig", Main.numberToWord(746));
         assertEquals("vier hundert acht und dreissig millionen sieben hundert sechs und zwanzig tausend fünf hundert acht und neunzig", Main.numberToWord(438726598));
         assertEquals("vier und siebzig", Main.numberToWord(74));
