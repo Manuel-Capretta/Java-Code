0d0bc7be8f1657f41430ab21f513798652600524
Manuel Capretta <familiecapretta@gmail.com>
2021-10-18 21:21:36 +0200

    bye bye

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        MainPanelGrafik p = new MainPanelGrafik();   // hier instanziieren wir das Panel Objekt (Zeichenfenster)
-
-        //p.paint(null);                // wir rufen die Zeichnungsfunktion auf.
-
-    }
-}
