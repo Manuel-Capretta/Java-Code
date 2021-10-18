7c29888f37810b08ab94f7b06654489b0c5eb1d8
Manuel Capretta <familiecapretta@gmail.com>
2021-10-18 17:49:44 +0200

    Created new class -> rect.java
    programm is now object oriented

--- rect calc/src/rect.java
+++ rect calc/src/rect.java
@@ -3,11 +3,11 @@ public class rect {
     int l;
     int w;
 
-    public int area(int l, int w){ //method for calculating area value
-        return l*w;
+    public int area(int l, int w) { //method for calculating area value
+        return l * w;
     }
 
-    public int scope(int l, int w){ //method for calculating scope value
-        return 2*(l*w);
+    public int scope(int l, int w) { //method for calculating scope value
+        return 2 * (l * w);
     }
 }
