ec7699865e125f8cc85f58681b2457dcf243ee76
Manuel Capretta <familiecapretta@gmail.com>
2021-10-18 17:54:08 +0200

    added color

--- rect calc/src/Main.java
+++ rect calc/src/Main.java
@@ -1,12 +1,18 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args){
         rect first_obj = new rect(); //generate new object
         first_obj.l = 2; //give values
         first_obj.w = 3;
 
+        first_obj.setColor(new Color(69));
+
         int area = first_obj.area(first_obj.l, first_obj.w); //calc area
         int scope = first_obj.scope(first_obj.l, first_obj.w); //calc scope
 
-        System.out.println("Area: " + area + "\n" + "Scope: " + scope);
+
+
+        System.out.println("Area: " + area + "\n" + "Scope: " + scope + "\n" + "Color: " + first_obj.getColor().toString());
     }
 }
--- rect calc/src/rect.java
+++ rect calc/src/rect.java
@@ -1,7 +1,11 @@
+import java.awt.*;
+
 //objects are always generated in a new class for simplifying and easy-to-use purposes
 public class rect {
     int l;
     int w;
+    Color color;
+
 
     public int area(int l, int w) { //method for calculating area value
         return l * w;
@@ -9,5 +13,13 @@ public class rect {
 
     public int scope(int l, int w) { //method for calculating scope value
         return 2 * (l * w);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
