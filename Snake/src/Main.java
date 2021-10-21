fe0a6109d6c5cd7f74fd66e4d504ffed9a8ced03
Manuel Capretta <familiecapretta@gmail.com>
2021-10-21 11:19:24 +0200

    snake can't slither into itself thus loses if done so
    flaws:
        - Apples can spawn in the snake

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        PlayingField p = new PlayingField();   // hier instanziieren wir das Panel Objekt (Zeichenfenster)
-
-        //p.paint(null);                // wir rufen die Zeichnungsfunktion auf.
-
+        PlayingField p = new PlayingField();
     }
 }
--- Snake/src/tile.java
+++ Snake/src/tile.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.awt.*;  //Abstract Window Toolkit
 
 public class tile {
